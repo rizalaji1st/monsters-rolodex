0a0cfa608267d6b9d6d2d33c6c6bfe85e3b96111
rizalaji1st <rizalaji561@gmail.com>
2022-01-18 13:44:40 +0700

    Binding function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends React.Component{
@@ -7,7 +8,8 @@ class App extends React.Component{
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -16,11 +18,21 @@ class App extends React.Component{
       .then(response => response.json())
       .then(users => this.setState({ monsters: users}));
     }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
   
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox 
+          placeholder='Search Monster'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
